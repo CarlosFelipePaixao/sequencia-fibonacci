914118489439e4b76c936e91ce3d1e7cc7566bf0
Carlos Felipe da Paixão <paixao.carlosfelipe@gmail.com>
2024-09-04 22:06:27 -0300

    Adicionar arquivos de projeto.

---
+++ SequenciaFibonacci/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+
+namespace SequenciaFibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Informe um número: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            if (PertenceSequenciaFibonacci(numero))
+            {
+                Console.WriteLine($"{numero} pertence à sequência de Fibonacci.");
+            }
+            else
+            {
+                Console.WriteLine($"{numero} não pertence à sequência de Fibonacci.");
+            }
+
+            Console.WriteLine("Pressione qualquer tecla para sair...");
+            Console.ReadKey();
+        }
+
+        static bool PertenceSequenciaFibonacci(int numero)
+        {
+            if (numero == 0 || numero == 1)
+            {
+                return true;
+            }
+
+            int a = 0, b = 1, c = 0;
+
+            while (c < numero)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+            }
+
+            return c == numero;
+
+
+
+        }
+    }
+}
